9c365e907517b3cacdbebf57788529b97c3caf4b
AmetoX <67999655+AmetoX@users.noreply.github.com>
2022-04-12 12:25:36 +0300

    Lab 12.04 (not finished)

---
+++ Poo_Alg/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Suma cifra / pb.info / 
+namespace Poo_Alg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n.SumaCifre());
+        }
+    }
+    public static class MyExtension 
+    { 
+        public static int SumCifra(this int n)
+        {
+            int suma= 0;
+            while()
+        }
+    }
+
+}
